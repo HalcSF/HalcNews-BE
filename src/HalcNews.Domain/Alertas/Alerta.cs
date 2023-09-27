9b348f8fac8d5b6ce70aa345a27657407e941efe
tobigrandi <tgrandi@fi.uba.ar>
2023-09-27 20:03:09 -0300

    Alerta y Notificacion

---
+++ src/HalcNews.Domain/Alertas/Alerta.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalcNews.Alertas
+{
+    public abstract class Alerta:
+    {
+        private string _Busqueda;
+        private DateOnly _FechaEncontrada;
+        private bool _Leida;
+
+        ///<sumary>
+        /// Crea una nueva Alerta
+        ///</sumary>
+        /// <param name="busqueda">Búsqueda relacionada a la alerta</param>
+        /// <param name="fechaEncontrada">Fecha que se encontró la búsqueda relacionada</param>
+        /// <param name="leida">Si la alerta fue o no leída por el usuario</param>
+        /// <returns>Alerta</returns>       
+
+        public Alerta(string busqueda, DateOnly fechaEncontrada, bool leida)
+        {
+            _Busqueda = busqueda;
+            _FechaEncontrada = fechaEncontrada;
+            _Leida = leida;
+        }
+
+        public string Busqueda { get => _Busqueda; set => _Busqueda = value; }
+        public DateOnly FechaEncontrada { get => _FechaEncontrada; set => _FechaEncontrada = value; }
+        public bool Leida { get => _Leida; set => _Leida = value; }
+    }
+}
