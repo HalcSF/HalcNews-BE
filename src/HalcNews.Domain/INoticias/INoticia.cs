c9ba50522e58a0866ce0d7abe8f12499f7565e4e
Alan <alanmaxit312@gmail.com>
2023-09-27 20:38:05 -0300

    dsadasdas

--- src/HalcNews.Domain/INoticias/INoticia.cs
+++ src/HalcNews.Domain/INoticias/INoticia.cs
@@ -5,9 +5,9 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace HalcNews.INoticia
+namespace HalcNews.INoticias
 {
-    public interface INoticias
+    public interface INoticia
     {
 
     }
