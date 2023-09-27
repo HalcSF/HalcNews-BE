564fc039df5cc0433ea0bf26e3bf416e67cd2837
Juan <juanignacioch4@gmail.com>
2023-09-27 20:19:23 -0300

    Clase Fuente

---
+++ src/HalcNews.Domain/Fuentes/Fuente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities;
+
+namespace HalcNews.Fuentes
+{
+    public class Fuente : Entity<int>
+    {
+        public string nombre {  get; set; }
+    }
+}
