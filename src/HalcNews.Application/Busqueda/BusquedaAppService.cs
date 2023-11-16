0b7c099ae649a0a21554e13db9641c14c9bc37cf
Juan Ignacio Chiappella Zarini <juanignacioch4@gmail.com>
2023-11-16 07:16:27 -0300

    Método SaveSearch. Guarda las noticias de una busqueda y las asocia con una NewsList

--- src/HalcNews.Application/Busqueda/BusquedaAppService.cs
+++ src/HalcNews.Application/Busqueda/BusquedaAppService.cs
@@ -1,11 +1,14 @@
 ﻿using HalcNews.ApiNews;
 using HalcNews.ListaNoticias;
+using HalcNews.Noticias;
+using HalcNews.News;
 using HalcNews.NewsList;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Volo.Abp.ObjectMapping;
 
 namespace HalcNews.Busqueda
 {
@@ -14,11 +17,13 @@ namespace HalcNews.Busqueda
         private readonly IApiNewsAppService _IApiNewsAppService;
         private readonly ISearchAppService _ISearchAppService;
         private readonly INewsListAppService _INewsListAppService;
-        public SearchAppService(IApiNewsAppService apiNewsAppService, ISearchAppService searchAppService, INewsListAppService newsListAppService )
+        private readonly INewAppService _INewAppService;
+        public SearchAppService(IApiNewsAppService apiNewsAppService, ISearchAppService searchAppService, INewsListAppService newsListAppService, INewAppService newAppService)
         {
             _IApiNewsAppService = apiNewsAppService;
             _ISearchAppService = searchAppService;
             _INewsListAppService = newsListAppService;
+            _INewAppService = newAppService;
         }
 
         public async Task<Search> SearchNews(string keyword)
@@ -27,15 +32,31 @@ namespace HalcNews.Busqueda
             var search = new Search();
 
             search.Keyword = keyword;
-            search.News = (ICollection<News.NewDto>)news;
+            search.News = news; //newsDto
             
             return search;
         }
-       // public async Task SaveSearchs(NewsListDto newsList, Search search)
-        //{
-            
-                // Para cada resultado de search.News, agregar a newsList.News.Add(new)
-                // Para esto, tengo que poder guardar las News como entidad New en la Db y asociarle a NewsList cada una de estas.
-        //}
+        public async Task SaveSearch(NewsListDto newsList, Search search)
+        {
+            // Para cada resultado de search.News, agregar a newsList.News.Add(new)
+            // Para esto, tengo que poder guardar las News como entidad New en la Db y asociarle a NewsList cada una de estas.
+            var newsResponse = search.News;
+
+            foreach (NewDto newE in newsResponse)
+            {
+                //agrega la entidad NewE a la base de datos
+                var newMapped = ObjectMapper.Map<NewDto,New>(newE);
+                await _INewAppService.InsertNewAync(newE);
+
+                //asocia la newsList con la newE
+                var newsListMapped = ObjectMapper.Map<NewsListDto, NewsListE>(newsList);
+                newsListMapped.News.Add(newMapped);
+
+                //actualiza la newsList en la base de datos
+                newsList = ObjectMapper.Map<NewsListE,NewsListDto >(newsListMapped);
+                await _INewsListAppService.UpdateNewsListAync(newsList);
+            }
+
+        }
     }
 }
